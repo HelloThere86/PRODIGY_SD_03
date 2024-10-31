43f9ce89a0558bf907b9ae3cda64f57d3a37804e
Ethan.Murire <Ethan.Murire@EHZ-LAP-EMURIRE>
2024-10-31 11:02:15 +0200

    Add project files.

---
+++ Prodigy Infotech task 3/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Prodigy_Infotech_task_3
+{
+    partial class ContactManagerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ContactManagerForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.phoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.contactsListBox = new System.Windows.Forms.ListBox();
+            this.DeleteContactButton = new System.Windows.Forms.Button();
+            this.AddContactButton = new System.Windows.Forms.Button();
+            this.UpdateContactButton = new System.Windows.Forms.Button();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Last Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Email:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Phone Number:";
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(209, 102);
+            this.lastNameTextBox.Multiline = true;
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(164, 29);
+            this.lastNameTextBox.TabIndex = 1;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(209, 220);
+            this.emailTextBox.Multiline = true;
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(164, 24);
+            this.emailTextBox.TabIndex = 1;
+            // 
+            // phoneNumberTextBox
+            // 
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(209, 158);
+            this.phoneNumberTextBox.Multiline = true;
+            this.phoneNumberTextBox.Name = "phoneNumberTextBox";
+            this.phoneNumberTextBox.Size = new System.Drawing.Size(162, 28);
+            this.phoneNumberTextBox.TabIndex = 1;
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(209, 44);
+            this.firstNameTextBox.Multiline = true;
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(164, 32);
+            this.firstNameTextBox.TabIndex = 1;
+            // 
+            // contactsListBox
+            // 
+            this.contactsListBox.FormattingEnabled = true;
+            this.contactsListBox.ItemHeight = 16;
+            this.contactsListBox.Location = new System.Drawing.Point(449, 44);
+            this.contactsListBox.Name = "contactsListBox";
+            this.contactsListBox.Size = new System.Drawing.Size(308, 372);
+            this.contactsListBox.TabIndex = 2;
+            this.contactsListBox.SelectedIndexChanged += new System.EventHandler(this.contactsListBox_SelectedIndexChanged);
+            // 
+            // DeleteContactButton
+            // 
+            this.DeleteContactButton.Location = new System.Drawing.Point(289, 309);
+            this.DeleteContactButton.Name = "DeleteContactButton";
+            this.DeleteContactButton.Size = new System.Drawing.Size(84, 36);
+            this.DeleteContactButton.TabIndex = 3;
+            this.DeleteContactButton.Text = "Delete";
+            this.DeleteContactButton.UseVisualStyleBackColor = true;
+            this.DeleteContactButton.Click += new System.EventHandler(this.DeleteContactButton_Click);
+            // 
+            // AddContactButton
+            // 
+            this.AddContactButton.Location = new System.Drawing.Point(56, 309);
+            this.AddContactButton.Name = "AddContactButton";
+            this.AddContactButton.Size = new System.Drawing.Size(75, 36);
+            this.AddContactButton.TabIndex = 3;
+            this.AddContactButton.Text = "Add ";
+            this.AddContactButton.UseVisualStyleBackColor = true;
+            this.AddContactButton.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // UpdateContactButton
+            // 
+            this.UpdateContactButton.Location = new System.Drawing.Point(163, 309);
+            this.UpdateContactButton.Name = "UpdateContactButton";
+            this.UpdateContactButton.Size = new System.Drawing.Size(88, 36);
+            this.UpdateContactButton.TabIndex = 3;
+            this.UpdateContactButton.Text = "Update";
+            this.UpdateContactButton.UseVisualStyleBackColor = true;
+            this.UpdateContactButton.Click += new System.EventHandler(this.UpdateContactButton_Click);
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Font = new System.Drawing.Font("Microsoft YaHei Light", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchTextBox.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.searchTextBox.Location = new System.Drawing.Point(43, 13);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(358, 25);
+            this.searchTextBox.TabIndex = 4;
+            this.searchTextBox.Text = "SEARCH";
+            this.searchTextBox.TextChanged += new System.EventHandler(this.searchTextBox_TextChanged);
+            // 
+            // ContactManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.UpdateContactButton);
+            this.Controls.Add(this.AddContactButton);
+            this.Controls.Add(this.DeleteContactButton);
+            this.Controls.Add(this.contactsListBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.phoneNumberTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ContactManagerForm";
+            this.Text = "Contact Manager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ContactManagerForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox phoneNumberTextBox;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.ListBox contactsListBox;
+        private System.Windows.Forms.Button DeleteContactButton;
+        private System.Windows.Forms.Button AddContactButton;
+        private System.Windows.Forms.Button UpdateContactButton;
+        private System.Windows.Forms.TextBox searchTextBox;
+    }
+}
+
---
+++ Prodigy Infotech task 3/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Prodigy_Infotech_task_3
+{
+    public partial class ContactManagerForm : Form
+    {
+        private List<Contact> contacts = new List<Contact>();
+        private string dataFile = "contacts.txt";
+
+        public ContactManagerForm()
+        {
+            InitializeComponent();
+            LoadContacts();
+            DisplayContacts();
+        }
+
+        private void UpdateContactButton_Click(object sender, EventArgs e)
+        {
+            if (contactsListBox.SelectedIndex != -1)
+            {
+                Contact selectedContact = contacts[contactsListBox.SelectedIndex];
+
+                // Update contact details
+                selectedContact.FirstName = firstNameTextBox.Text;
+                selectedContact.LastName = lastNameTextBox.Text;
+                selectedContact.PhoneNumber = phoneNumberTextBox.Text;
+                selectedContact.Email = emailTextBox.Text;
+
+                DisplayContacts();
+                MessageBox.Show("Contact updated successfully.");
+            }
+            else
+            {
+                MessageBox.Show("Please select a contact to update.");
+            }
+        }
+
+        private void DeleteContactButton_Click(object sender, EventArgs e)
+        {
+            if (contactsListBox.SelectedIndex != -1)
+            {
+                contacts.RemoveAt(contactsListBox.SelectedIndex);
+                DisplayContacts();
+                MessageBox.Show("Contact deleted successfully.");
+            }
+            else
+            {
+                MessageBox.Show("Please select a contact to delete.");
+            }
+        }
+
+        private void contactsListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (contactsListBox.SelectedIndex != -1)
+            {
+                Contact selectedContact = contacts[contactsListBox.SelectedIndex];
+                firstNameTextBox.Text = selectedContact.FirstName;
+                lastNameTextBox.Text = selectedContact.LastName;
+                phoneNumberTextBox.Text = selectedContact.PhoneNumber;
+                emailTextBox.Text = selectedContact.Email;
+            }
+        }
+
+        private void DisplayContacts()
+        {
+            contactsListBox.Items.Clear();
+            var sortedContacts = contacts.OrderBy(c => c.FirstName).ThenBy(c => c.LastName);
+            foreach (Contact contact in sortedContacts)
+            {
+                contactsListBox.Items.Add($"{contact.FirstName} {contact.LastName}");
+            }
+        }
+
+        private void LoadContacts()
+        {
+            try
+            {
+                if (File.Exists(dataFile))
+                {
+                    using (StreamReader reader = new StreamReader(dataFile))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            string[] parts = line.Split(',');
+                            if (parts.Length == 4)
+                            {
+                                Contact contact = new Contact(parts[0], parts[1], parts[2], parts[3]);
+                                contacts.Add(contact);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading contacts: {ex.Message}");
+            }
+        }
+
+        private void SaveContacts()
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(dataFile))
+                {
+                    foreach (Contact contact in contacts)
+                    {
+                        writer.WriteLine($"{contact.FirstName},{contact.LastName},{contact.PhoneNumber},{contact.Email}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error saving contacts: {ex.Message}");
+            }
+        }
+
+        private void ContactManagerForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveContacts();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string firstName = firstNameTextBox.Text;
+            string lastName = lastNameTextBox.Text;
+            string phoneNumber = phoneNumberTextBox.Text;
+            string email = emailTextBox.Text;
+
+            if (!IsValidEmail(email))
+            {
+                MessageBox.Show("Invalid email format.");
+                return;
+            }
+
+            if (!IsValidPhoneNumber(phoneNumber))
+            {
+                MessageBox.Show("Invalid phone number format.");
+                return;
+            }
+
+            if (contacts.Any(c => c.Email == email || c.PhoneNumber == phoneNumber))
+            {
+                MessageBox.Show("A contact with this email or phone number already exists.");
+                return;
+            }
+
+            Contact newContact = new Contact(firstName, lastName, phoneNumber, email);
+            contacts.Add(newContact);
+
+            ClearInputFields();
+            DisplayContacts();
+            MessageBox.Show("Contact added successfully.");
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private bool IsValidPhoneNumber(string phoneNumber)
+        {
+            return Regex.IsMatch(phoneNumber, @"^\+?[1-9]\d{1,14}$");
+        }
+
+        private void ClearInputFields()
+        {
+            firstNameTextBox.Clear();
+            lastNameTextBox.Clear();
+            phoneNumberTextBox.Clear();
+            emailTextBox.Clear();
+        }
+
+        private void searchTextBox_TextChanged(object sender, EventArgs e)
+        {
+            string searchTerm = searchTextBox.Text.ToLower();
+            contactsListBox.Items.Clear();
+            var filteredContacts = contacts
+                .Where(c => c.FirstName.ToLower().Contains(searchTerm) || c.LastName.ToLower().Contains(searchTerm))
+                .OrderBy(c => c.FirstName)
+                .ThenBy(c => c.LastName);
+
+            foreach (Contact contact in filteredContacts)
+            {
+                contactsListBox.Items.Add($"{contact.FirstName} {contact.LastName}");
+            }
+        }
+    }
+
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Contact(string firstName, string lastName, string phoneNumber, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+}
